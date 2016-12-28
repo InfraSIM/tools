a90761ad8854c4e0b573b3eff8ebb577ad5b3880
XiaowenJiang <hehejxw@163.com>
2016-12-28 04:37:22 -0800

    Modify default yaml file for new CLI 1) Specify "name" in yaml file. 2) Move default yaml file to .node_map directory.

--- packer/scripts/infrasim-compute.sh
+++ packer/scripts/infrasim-compute.sh
@@ -18,4 +18,4 @@ sleep 1
 
 # init infrasim service
 infrasim init
-wget https://raw.githubusercontent.com/InfraSIM/tools/master/packer/scripts/infrasim.yml -O /usr/local/infrasim/etc/infrasim.yml -q
+wget https://raw.githubusercontent.com/InfraSIM/tools/master/packer/scripts/infrasim.yml -O /home/infrasim/.infrasim/.node_map/default.yml -q
