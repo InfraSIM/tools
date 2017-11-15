d9d4cbba4c8fe93b8fa93be1afe98d206f4fe9d0
JuneZhou19 <june.zhou@emc.com>
2017-11-15 05:51:49 +0000

    fix qemu version display

--- deb_builder/qemu-package.sh
+++ deb_builder/qemu-package.sh
@@ -10,7 +10,7 @@ die() {
 }
 
 usage() {
-    echo "$0 [--source-path] [--pkgname] [--pkgversion] [--pkgrelease]"
+    echo "$0 [--source-path] [--pkgname] [--pkgversion] [--build-number] [--pkgrelease]"
     exit 0
 }
 
@@ -20,7 +20,6 @@ JOBS=32
 
 [ -n "$CHECKINSTALL" ] || die "Please install checkinstall."
 
-pkgrelease=1
 src=""
 while [ $# -gt 0 ]; do
     option=$1
@@ -28,6 +27,7 @@ while [ $# -gt 0 ]; do
     --source-path) src=$2;shift ;;
     --pkgname) pkgname=$2;shift ;;
     --pkgversion) pkgversion=$2;shift ;;
+    --build-number) buildnumber=$2;shift;;
     --pkgrelease) pkgrelease=$2;shift ;;
     --help) usage ;;
     esac
@@ -40,26 +40,36 @@ done
 [ -n "$pkgname" ] || die "--pkgname is required."
 [ -n "$pkgversion" ] || die "--pkgversion is required."
 
-pushd $src
+
+[ ! -d ${src}/build ] && mkdir -p ${src}/build
+pushd $src/build
+
+distribution=$(lsb_release -i | cut -d: -f2 | sed 's|^\s||g' | tr '[:upper:]' '[:lower:]')
+os_dist_name=$(lsb_release -c | cut -d: -f2 | sed 's|^\s||g')
+
+pkgversion=${pkgversion}-${distribution}-${os_dist_name}
 
 #[ -f Makefile ] && make distclean
-./configure --prefix=/usr/local --target-list=${ARCH}-linux-user,${ARCH}-softmmu \
-    --disable-smartcard --disable-seccomp --disable-glusterfs --disable-tpm \
+../configure --prefix=/usr/local --target-list=${ARCH}-linux-user,${ARCH}-softmmu \
+    --disable-smartcard --disable-seccomp --disable-glusterfs --with-pkgversion="$pkgname ${distribution}-${os_dist_name} build-$buildnumber" --disable-tpm \
     --disable-vhdx --disable-bluez --disable-gtk --disable-cocoa --disable-sdl --disable-xen \
-    --without-system-pixman --enable-numa --enable-linux-aio
+    --without-system-pixman
 make -j${JOBS}
 
 #sudo apt-get build-dep libxen-dev
 
 $CHECKINSTALL -D -y \
+    -d2 \
     --install=no \
     --fstrans=no \
+    --deldoc=yes \
+    --gzman=yes \
     --reset-uids=yes \
     --pkgname=$pkgname \
     --pkgversion=$pkgversion \
     --pkgrelease=$pkgrelease \
     --exclude="/home" \
-    --maintainer="InfraSIM Team <infrasim@googlegroups.com>" \
+    --maintainer="infrasim@googlegroups.com" \
     make install
 
 popd
